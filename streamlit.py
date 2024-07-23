36fdb71c863a45d58bc9e1bdfa858ff40ea39292
Mishra2203 <smtmadhumishra2@gmail.com>
2024-07-23 17:06:38 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,11 +1,22 @@
 import streamlit as st
-name=st.text_input['Enter your name']
-st.write("Hello",name)
-st.subheader("chosoe your gender")
-st.radio('select',('male','female'))
-st.subheader("enter your current education")
-st.selectbox("education:",['10','12','Btech','Mbbs','others','nalla'])
-like=st.slider('you like this site',1,100,step=1)
-st.write('thanks for your answer',like)
-if(st.button('i am not a robot')):
- st.write("i know you are human")
+
+# Get user's name
+name = st.text_input('Enter your name')
+st.write(f"Hello, {name}!")
+
+# Get user's gender
+st.subheader("Choose your gender")
+gender = st.radio('Select', ['Male', 'Female'])
+
+# Get user's current education
+st.subheader("Enter your current education")
+education = st.selectbox("Education:", ['10', '12', 'B.Tech', 'MBBS', 'Others', 'N/A'])
+
+# Get user's liking for the site
+like = st.slider('How much do you like this site?', 1, 100, step=1)
+st.write(f'Thanks for your answer: {like}')
+
+# Check if user is not a robot
+if st.button('I am not a robot'):
+    st.write("I know you are human!")
+
