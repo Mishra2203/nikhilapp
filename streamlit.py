8fe508c892003683a60bdeca2f8fd60af5795ab0
Mishra2203 <smtmadhumishra2@gmail.com>
2024-07-23 22:10:01 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,7 @@
 import streamlit as st
- st.chat_message("user"):
-    st.write("Hello 👋")
-    st.line_chart(np.random.randn(30, 3))
+st.chat_message("user"):
+st.write("Hello 👋")
+st.line_chart(np.random.randn(30, 3))
 
 # Display a chat input widget.
 >>> st.chat_input("Say something")
