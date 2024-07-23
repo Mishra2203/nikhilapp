b728cb9136322f2ecb8ef3f3c38d834126c13349
Mishra2203 <smtmadhumishra2@gmail.com>
2024-07-23 17:14:12 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,13 +4,17 @@ import streamlit as st
 name = st.text_input('Enter your name')
 st.write(f"Hello, {name}!")
 
+st.subheader("Enter your age")
+st.number_input('Select your age',0,100)
+st.date_input('Date')
+
 # Get user's gender
 st.subheader("Choose your gender")
-gender = st.radio('Select', ['Male', 'Female'])
+gender = st.radio('Select', ['Not preffered to say','Male', 'Female','Others'])
 
 # Get user's current education
 st.subheader("Enter your current education")
-education = st.selectbox("Education:", ['10', '12', 'B.Tech', 'MBBS', 'Others', 'N/A'])
+education = st.selectbox("Education:", ['10', '12', 'B.Tech', 'MBBS', 'Others', 'NAllA'])
 
 # Get user's liking for the site
 like = st.slider('How much do you like this site?', 1, 100, step=1)
@@ -19,4 +23,7 @@ st.write(f'Thanks for your answer: {like}')
 # Check if user is not a robot
 if st.button('I am not a robot'):
     st.write("I know you are human!")
+
+text area=st.text_area('Write something about yourself')
+st.write(text area)
 
