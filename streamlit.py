5ccb8d85fe3b66b0b57e8a32f02624ae6f47a5e4
Mishra2203 <smtmadhumishra2@gmail.com>
2024-07-24 11:19:55 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -12,6 +12,6 @@ st.header("JAAT,PANDIT,GURJAR,THAKUR")
 
 video_file = st.file_uploader("Upload the song you want to hear and after that fill below all the details", type=['mp4'])
 if video_file is not None:
-    st.video(video_file.read())
+    st.video(video_file.read(),5)
 
 
