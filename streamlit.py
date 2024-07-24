99903f9163a130ca0109c28f2a2627621782348d
Mishra2203 <smtmadhumishra2@gmail.com>
2024-07-24 11:31:28 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,7 +4,7 @@ st.button("Enter IN UTTAR PRADESH")
 st.title("UTTAR PRADESH")
 st.header("JAAT,PANDIT,GURJAR,THAKUR")
 img = Image.open('D:\\NIKHIL\\up.jpg')
-    st.image(img)
+st.image(img)
 video_file = st.file_uploader("Upload the song you want to hear and after that fill below all the details", type=['mp4'])
 if video_file is not None:
     st.video(video_file.read())
