299c44a8163dc62085c17ea0a02fe4d0001f27fc
Mishra2203 <smtmadhumishra2@gmail.com>
2024-07-24 11:51:46 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,18 @@
 import streamlit as st
 st.title("UTTAR PRADESH")
-st.header("JAAT,PANDIT,GURJAR,THAKUR")
+st.header("{JAAT},{PANDIT}")
+
+image_file = st.file_uploader("D:\NIKHIL\up.jpg", type=['jpg', 'png'])
+image_file = st.file_uploader("D:\NIKHIL\pandit.jpeg", type=['jpeg', 'png'])
+image_file = st.file_uploader("D:\NIKHIL\jaat.jpeg", type=['jpeg', 'png'])
+
+
+if image_file is not None:
+    img = Image.open(image_file)
+    st.image(img)
+
+
+
 video_file = st.file_uploader("Upload the song you want to hear and after that fill below all the details", type=['mp4'])
 st.video(video_file,start_time=5)
 
