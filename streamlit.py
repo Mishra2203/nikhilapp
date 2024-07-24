3ef91c03ba9dfb7cf6a50c103378ac4152bfb25b
Mishra2203 <smtmadhumishra2@gmail.com>
2024-07-24 11:35:40 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from PIL import Image
-img = Image.open('D:\\NIKHIL\\up.jpg')
+img = Image.open('D:/NIKHIL/up.jpg')
 st.image(img)
 st.button("Enter IN UTTAR PRADESH")
 st.title("UTTAR PRADESH")
