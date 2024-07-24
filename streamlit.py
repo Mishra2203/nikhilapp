5e133851e1264c560ef7e747e7bdfc1b241238dd
Mishra2203 <smtmadhumishra2@gmail.com>
2024-07-24 11:40:15 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -3,6 +3,7 @@ if st.button("Enter IN UTTAR PRADESH"):
  st.title("UTTAR PRADESH")
  st.header("JAAT,PANDIT,GURJAR,THAKUR")
  video_file = st.file_uploader("Upload the song you want to hear and after that fill below all the details", type=['mp4'])
+ st.video(video_file.read())
 
 
 
