79bbc75bf1ab89dc4daae3612e9b9ca5ca24359f
Mishra2203 <smtmadhumishra2@gmail.com>
2024-07-24 11:42:46 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,9 +1,8 @@
 import streamlit as st
-
- st.title("UTTAR PRADESH")
- st.header("JAAT,PANDIT,GURJAR,THAKUR")
- video_file = st.file_uploader("Upload the song you want to hear and after that fill below all the details", type=['mp4'])
-  st.video(video_file,start_time=5)
+st.title("UTTAR PRADESH")
+st.header("JAAT,PANDIT,GURJAR,THAKUR")
+video_file = st.file_uploader("Upload the song you want to hear and after that fill below all the details", type=['mp4'])
+ st.video(video_file,start_time=5)
 
 
 
