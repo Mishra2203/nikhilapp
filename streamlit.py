521f4f4352f33092f6084bf37b628789dddbc4c7
Mishra2203 <smtmadhumishra2@gmail.com>
2024-07-24 11:36:33 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from PIL import Image
-img = Image.open('D:/NIKHIL/up.jpg')
-st.image(img)
+
 st.button("Enter IN UTTAR PRADESH")
-st.title("UTTAR PRADESH")
-st.header("JAAT,PANDIT,GURJAR,THAKUR")
-video_file = st.file_uploader("Upload the song you want to hear and after that fill below all the details", type=['mp4'])
+if button:
+ st.title("UTTAR PRADESH")
+ st.header("JAAT,PANDIT,GURJAR,THAKUR")
+ video_file = st.file_uploader("Upload the song you want to hear and after that fill below all the details", type=['mp4'])
 
 
 
